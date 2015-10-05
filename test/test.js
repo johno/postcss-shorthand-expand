a0dd55cc558ec0f4a3f024a71e40ba2f31e169ef
John Otander <johnotander@gmail.com>
2015-10-05 16:14:45 -0600

    Add failing test

--- test/test.js
+++ test/test.js
@@ -1,10 +1,22 @@
 'use strict'
 
+var fs = require('fs')
 var test = require('tape')
+var postcss = require('postcss')
 var postcssRemoveShorthands = require('..')
 
 test('postcss-remove-shorthands', function (t) {
-  t.plan(1)
-
-  t.equal(postcssRemoveShorthands(), true)
+  testFixture(t, 'input.css', 'output.css')
 })
+
+function fixture (name) {
+    return fs.readFileSync('test/fixtures/' + name, 'utf8')
+}
+
+function testFixture (t, input, output) {
+    t.equal(
+      postcss([ postcssRemoveShorthands() ])
+        .process(fixture(input)).css,
+      fixture(output)
+    )
+}
